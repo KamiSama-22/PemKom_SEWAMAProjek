eae06d1ce8c0909391083cd84717bdad9d58ccb6
Tuanmuda220406 <muhammad.irhas108@gmail.com>
2026-04-22 23:02:07 +0700

    first commit

---
+++ src/main/java/com/mycompany/sewama_project/SEWAMA_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sewama_project;
+
+/**
+ *
+ * @author ADVAN
+ */
+public class SEWAMA_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
